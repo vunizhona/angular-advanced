1770981ff6c121f1f5efcac838a68bb1bfbc38a4
Victoria <victoria.unizhona@techmagic.co>
2024-05-28 21:13:31 +0300

    rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CommentsComponent} from "./comments/comments.component";
+import {CommentsCheckerComponent} from "./comments-checker/comments-checker.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommentsComponent, CommentsCheckerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
